82b3de36456ed0c62150609910e54cebfab88dae
wbs <1134954328@qq.com>
2023-12-14 16:48:37 +0800

    更新依赖版本

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,10 +1,8 @@
-import { faker } from '@faker-js/faker'
 import type { MockMethod } from 'vite-plugin-mock'
 import { MockSession } from './MockSession'
 import { MockCourseList } from './mockCourseList'
 import { MockMe } from './mockMe'
 
-faker.setLocale('zh_CN')
 
 export default [
   ...MockMe, ...MockSession, ...MockCourseList
