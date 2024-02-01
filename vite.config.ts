25379922048f2090f8624ca814c53eae9394fc72
wbs <1134954328@qq.com>
2024-02-01 16:20:23 +0800

    更新依赖，tailwind 添加隐藏滚动条样式

--- vite.config.ts
+++ vite.config.ts
@@ -31,10 +31,9 @@ export default defineConfig(() => {
         include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/],
         dts: '/src/types/components.d.ts',
       }),
-
     ],
     server: {
-      open: true,
+      open: false, // 自动打开浏览器
       host: true,
       // proxy: {
       //   '/api/v1': {
@@ -59,6 +58,14 @@ export default defineConfig(() => {
           }
         }
       }
+    },
+    esbuild: {
+      // 打包时移除 console.log
+      pure: ['console.log'],
+      // 打包时移除 debugger
+      drop: ['debugger'],
+      // 打包时移除所有注释
+      legalComments: 'none'
     },
   }
 })
