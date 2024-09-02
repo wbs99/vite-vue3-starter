e257e55f7e5a88c19c61731d4de52d4e01eff7ae
wbs <1134954328@qq.com>
2024-09-02 15:11:06 +0800

    更新 tailwind 配置说明

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ export default {
         primary: '#5c33be',
       },
     },
+    // 可以在这定义断点的值,目前都是默认值
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [
     function ({ addVariant }) {
