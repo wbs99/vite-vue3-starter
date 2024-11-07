8655950a5c194b4a55367f95a6cb34369a689bb2
wbs <1134954328@qq.com>
2024-11-07 15:21:41 +0800

    更新 api 和依赖

--- src/api/tagApi.ts
+++ src/api/tagApi.ts
@@ -9,33 +9,31 @@ export type Tag = {
 
 export type TagParams = Partial<Tag & Pager>
 
-export const FETCH_TAG_QUERY_KEY = 'tagApi'
-export const FETCH_TAG_LIST_QUERY_KEY = 'tagListApi'
+export const GET_TAG_QUERY_KEY = 'GET_TAG_QUERY_KEY'
+export const GET_TAG_LIST_QUERY_KEY = 'FETCH_TAG_LIST_QUERY_KEY'
 
-export const fetchTagApi = (tagId: number) => http.get<Resource<Tag>>(`/tag/${tagId}`)
-
-export const useFetchTag = (me: Ref<User | undefined>, tagId: number) => {
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: [FETCH_TAG_QUERY_KEY, me, tagId],
+export const getTagApi = (tagId: number) => http.get<Resource<Tag>>(`/tag/${tagId}`)
+export const useGetTag = (me: Ref<User | undefined>, tagId: number) => {
+  const { isPending, data } = useQuery({
+    queryKey: [GET_TAG_QUERY_KEY, me, tagId],
     queryFn: async () => {
-      const response = await fetchTagApi(tagId)
+      const response = await getTagApi(tagId)
       return response.data.resource
     },
     enabled: computed(() => !!me.value?.id)
   })
   return {
-    tag: data, isTagPending: isPending, isTagError: isError, tagError: error,
+    tag: data, isTagPending: isPending
   }
 }
 
-export const fetchTagListApi = (data: Pager) => http.get<Resources<Tag>>('/tags', data)
-
-export const useFetchTagList = (params: Pager) => {
+export const getTagListApi = (data: Pager) => http.get<Resources<Tag>>('/tags', data)
+export const useGetTagList = (params: Pager) => {
   const { currentPage, perPage } = params
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError, isPending } = useInfiniteQuery({
-    queryKey: [FETCH_TAG_LIST_QUERY_KEY],
+    queryKey: [GET_TAG_LIST_QUERY_KEY],
     queryFn: async ({ pageParam = currentPage }) => {
-      const response = await fetchTagListApi({ ...params, currentPage: pageParam })
+      const response = await getTagListApi({ ...params, currentPage: pageParam })
       return response.data
     },
     initialPageParam: currentPage,
@@ -50,30 +48,27 @@ export const useFetchTagList = (params: Pager) => {
 }
 
 export const addTagApi = (data: TagParams) => http.post<Resource<Tag>>('/tag', data)
-
 export const useAddTag = () => {
   const queryClient = useQueryClient()
   const { isPending, isError, error, isSuccess, mutate } = useMutation({
     mutationFn: (newTag: TagParams) => addTagApi(newTag),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [FETCH_TAG_LIST_QUERY_KEY] })
+      queryClient.invalidateQueries({ queryKey: [GET_TAG_LIST_QUERY_KEY] })
     }
   })
-
   return {
     isAddTagPending: isPending, isAddTagError: isError, addTagError: error, isAddTagSuccess: isSuccess, addTag: mutate
   }
 }
 
 export const updateTagApi = (newTag: TagParams) => http.patch<Resource<Tag>>(`/tag/${newTag.id}`, newTag)
-
 export const useUpdateTag = () => {
   const queryClient = useQueryClient()
   const { isPending, isError, error, isSuccess, mutate } = useMutation({
     mutationFn: (newTag: TagParams) => updateTagApi(newTag),
     onSuccess: (data, variables) => {
       console.log('这里触发了')
-      queryClient.setQueryData([FETCH_TAG_QUERY_KEY, { id: variables.id }], {...data, tagName: variables.tagName})
+      queryClient.setQueryData([GET_TAG_QUERY_KEY, { id: variables.id }], {...data, tagName: variables.tagName})
     }
   })
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,7 +10,6 @@
 
     <div>
       <span v-if="isTagPending">正在获取标签信息...</span>
-      <span v-else-if="isTagError">获取标签信息失败: {{ tagError }}</span>
       <div v-else class="font-bold tracking-tight text-transparent bg-gradient-to-r from-emerald-400 to-green-600 bg-clip-text dark:from-green-200 dark:to-green-400">
         <p>这是标签名称 -- {{ tag?.tagName }}</p>
         <p>这是标签id -- {{ tag?.id }}</p>
@@ -70,11 +69,11 @@
 import type { TagParams } from '../api/tagApi'
 import { useIntersectionObserver } from '@vueuse/core'
 import { useFetchMe } from '../api/meApi'
-import { useAddTag, useFetchTag, useFetchTagList, useUpdateTag } from '../api/tagApi'
+import { useAddTag, useGetTag, useGetTagList, useUpdateTag } from '../api/tagApi'
 
 const {meData, isMePending, isMeError, meError} = useFetchMe()
 
-const {tag, isTagPending, isTagError, tagError} = useFetchTag(meData, 6)
+const {tag, isTagPending} = useGetTag(meData, 6)
 
 const {isAddTagPending, isAddTagError, addTagError, isAddTagSuccess, addTag} = useAddTag()
 
@@ -88,7 +87,7 @@ const onUpdateTag = (data: TagParams) => {
   updateTag(data)
 }
 
-const { tagList, fetchMoreTag, hasMoreTag, isFetchingMoreTag, isFetchTagError, isFetchingTagPending } = useFetchTagList({currentPage: 1, perPage: 20 })
+const { tagList, fetchMoreTag, hasMoreTag, isFetchingMoreTag, isFetchTagError, isFetchingTagPending } = useGetTagList({currentPage: 1, perPage: 20 })
 
 const flattenedTagList = computed(() => tagList.value?.pages?.flatMap(page => page.resources) || [])
 
