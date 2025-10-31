d17254cefc4575025cd838b94cd1357edf344d9f
wbs <1134954328@qq.com>
2025-10-31 12:15:30 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,21 @@
 </template>
 
 <script setup lang="ts">
+import autofit from 'autofit.js'
+
+onMounted(() => {
+  autofit.init({
+    dh: 1080,
+    dw: 1920,
+    el: '#app',
+    resize: true,
+  })
+  autofit.isAutofitRunning = true
+})
+
+onUnmounted(() => {
+  autofit.off()
+})
 </script>
 
 <style lang="scss">
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="w-40 h-40 bg-purple-500" @touchstart="onTouchStart" @touchmove="onTouchMove" @touchend="onTouchEnd">
-    长按我{{ meStore.me.email }}
+  <div id="container" class="flex flex-col bg-purple-500 size-full">
+    <div class="w-[400px] h-[340px] bg-red-500">
+      1
+    </div>
+    <div class="w-[400px] h-[340px] bg-blue-500">
+      2
+    </div>
+    <div class="w-[400px] h-[340px] bg-green-500">
+      3
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useLongPress } from '../hooks/useLongPress'
-import { useMeStore } from '../stores/meStore'
 
-const { onTouchStart, onTouchMove, onTouchEnd } = useLongPress(() => {
-  console.log('触发长按')
-}, 600)
-
-const meStore = useMeStore()
 </script>
+
+<style lang="scss" scoped>
+
+</style>
